a209ac4817816155a4df136eac0084ef40d025e9
umfrancisco <francis.guitler@gmail.com>
2026-02-03 09:02:29 -0300

    added new class MenuOptions

--- src/com/umfrancisco/Main.java
+++ src/com/umfrancisco/Main.java
@@ -1,10 +1,8 @@
 package com.umfrancisco;
 
-import java.util.Random;
 import java.util.Scanner;
-
 import com.umfrancisco.bank.Bank;
-import com.umfrancisco.bank.Customer;
+import com.umfrancisco.menu.MenuOptions;
 
 public class Main {
 	public static void main(String[] args) {
@@ -29,93 +27,8 @@ public class Main {
 					""";
 		
 		Scanner scanner = new Scanner(System.in);
-		Bank bank = showBankOptions(scanner, bankMenu, banks);
-		showCustomerOptions(scanner, customerMenu, bank);
+		Bank bank = MenuOptions.showBankOptions(scanner, bankMenu, banks);
+		MenuOptions.showCustomerOptions(scanner, customerMenu, bank);
 		scanner.close();
-	}
-	
-	public static Bank showBankOptions(Scanner scanner, String bankMenu, Bank[] banks) {
-		while (true) {
-			System.out.print(bankMenu);
-			String option = scanner.nextLine();
-			System.out.println("-".repeat(40));
-			
-			if (option.equals("100")) {
-				return banks[0];
-			} else if (option.equals("200")) {
-				return banks[1];
-			} else if (option.equals("300")) {
-				return banks[2];
-			} else {
-				System.out.println("Bank number not found...");
-			}
-			
-		}
-	}
-	
-	public static void showCustomerOptions(Scanner scanner, String customerMenu, Bank bank) {
-		
-		String accountManagementMenu = """
-				Choose one option:
-					1 - Deposit
-					2 - Withdraw
-					3 - Return
-					""";
-		
-		while (true) {
-			System.out.print("Welcome to "+bank.getName()+" Bank\n\n");
-			System.out.print(customerMenu);
-			String option = scanner.nextLine();
-			System.out.println("-".repeat(40));
-			
-			if (option.equals("1")) {
-				System.out.print("Enter your name: ");
-				String name = scanner.nextLine();
-				System.out.print("Enter a amount: ");
-				String amount = scanner.nextLine();
-				int randomGeneratedId = new Random().nextInt(1000);
-				bank.newBankCustomer(randomGeneratedId, name, Double.parseDouble(amount));
-				System.out.println("\nYour bank number is "+randomGeneratedId);
-			} else if (option.equals("2")) {
-				System.out.println("Existing customer\n");
-				System.out.print("Enter your bank number: ");
-				String id = scanner.nextLine();
-				Customer customer = bank.getCustomer(Integer.parseInt(id));
-				showAccountManagementOptions(scanner, accountManagementMenu, customer, bank);
-			} else if (option.equals("3")) {
-				System.out.println("Goodbye");
-				break;
-			} else {
-				System.out.println("Try again...");
-			}
-			System.out.println("-".repeat(40));
-		}
-	}
-	
-	public static void showAccountManagementOptions(Scanner scanner, String accountManagementMenu, Customer customer, Bank bank) {
-		while (true) {
-			System.out.println("\n"+customer);
-			System.out.print(accountManagementMenu);
-			
-			String option = scanner.nextLine();
-			if (option.equals("1")) {
-				System.out.println("-".repeat(40));
-				System.out.print("Deposit\n\nEnter a value to deposit: ");
-				String value = scanner.nextLine();
-				boolean result = bank.deposit(customer, Double.parseDouble(value));
-				if (result) {
-					System.out.println("Deposit successful!!");
-				} else {
-					System.out.println("Something went wrong...");
-				}
-				System.out.println("-".repeat(40));
-			} else if (option.equals("2")) {
-				System.out.println("Withdraw");
-			} else if (option.equals("3")) {
-				break;
-			} else {
-				System.out.println("Try again...");
-			}
-		}
 	}
 }
